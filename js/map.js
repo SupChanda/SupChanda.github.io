743f3a90231743c8ba71499a46013b380bd59d2e
Supratik Chanda <supchanda08kol50@gmail.com>
2019-12-08 23:19:58 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -238,19 +238,19 @@ class Map{
 								   d3.select("#year"+ yearList[k].toString()).attr('r',6).attr('fill','red')
 							   }
 						   }
-							console.log('yess I have selected i value: ',i)
+							//console.log('yess I have selected i value: ',i)
 							let year=1990+i;
 							if (optionSelected=='HDI'){
 								
 							let color = d3.scaleLinear()
 							  .domain([0,1])
 							.range([0,255]);
-							for (let j=0;j<data.length;j++){
+							for (let j=0;j<this.hdi.length;j++){
 								 
 								////console.log(j,data[j].Country_Code,color(parseFloat(data[j][year.toString()])));
-								d3.select('#'+data[j].Country_Code)
+								d3.select('#'+this.hdi[j].Country_Code)
 								
-								.attr('fill',"rgb(" +"0, " + color(parseFloat(data[j][year.toString()]))   + ",0 )");
+								.attr('fill',"rgb(" +"0, " + color(parseFloat(this.hdi[j][year.toString()]))   + ",0 )");
 							}
 							
 							}
